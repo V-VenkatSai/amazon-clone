f898319a7bd3827703bf3f71a90b536664aadd3e
Venkat Sai <venkatsai1428@gmail.com>
2024-04-20 21:47:32 +0530

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -22,7 +22,7 @@ cart.forEach((cartItem) => {
   const dateString = calculateDeliveryDate(deliveryOption);
 
 cartSummaryHTML += ` 
-    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+    <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
                 <div class="delivery-date">
                   Delivery date: ${dateString}
                 </div>
@@ -38,7 +38,7 @@ cartSummaryHTML += `
                     <div class="product-price">
                       $${formatCurrency(matchingProduct.priceCents)}
                     </div>
-                    <div class="product-quantity">
+                    <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                       <span>
                         Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                       </span>
@@ -47,7 +47,7 @@ cartSummaryHTML += `
                       </span>
                       <input class="quantity-input js-quantity-input-${matchingProduct.id}" type="number" value="${cartItem.quantity}">
                       <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
-                      <span  class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                      <span  class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                         Delete
                       </span>
                     </div>
